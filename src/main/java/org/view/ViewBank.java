1292d6cef14481e345bcf93efadc11a43fef87e3
Samuel Dias Oliveira <samueldias004@gmail.com>
2026-01-26 20:59:00 -0300

    feat: adapt view to new user-based business logic

--- src/main/java/org/view/ViewBank.java
+++ src/main/java/org/view/ViewBank.java
@@ -1,16 +1,24 @@
 package org.view;
 
+import org.exceptions.InvalidUserFileException;
 import org.exceptions.PersonalizedException;
 import org.interfaces.AccountRepository;
 import org.model.Account;
+import org.model.User;
 import org.repository.InMemoryAccountRepository;
 import org.service.BankService;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.*;
+import java.util.List;
 import java.util.Scanner;
 
 public class ViewBank {
     Scanner scanner = new Scanner(System.in);
     BankService bankService;
+    User loggedUser;
 
     public ViewBank(BankService bankService) {
         this.bankService = bankService;
@@ -20,7 +28,8 @@ public class ViewBank {
         AccountRepository repository = new InMemoryAccountRepository();
         BankService service = new BankService(repository);
         ViewBank view = new ViewBank(service);
-
+        view.loggedUser = getUserFromMemory();
+        System.out.println(view.loggedUser);
         view.start();
     }
 
@@ -51,11 +60,32 @@ public class ViewBank {
         System.out.println("Aplicação encerrada.");
     }
 
+    // ---- USER INFO ----
+    private static User getUserFromMemory() {
+        Path userDocument = Path.of("src/main/java/org/document/user-document.txt");
+        try (BufferedReader bufferedReader = Files.newBufferedReader(userDocument, StandardCharsets.UTF_8);) {
+            String line = null;
+            while ((line = bufferedReader.readLine()) != null) {
+                String[] docs = line.split(",");
+                if (docs.length == 3) {
+                    return User.UserBuilder.newBuilder()
+                                            .setFirstName(docs[0].trim())
+                                            .setLastName(docs[1].trim())
+                                            .setCpf(docs[2].trim())
+                                            .build();
+                }
+            }
+            throw new InvalidUserFileException();
+        } catch (IOException | InvalidUserFileException e) {
+            throw new InvalidUserFileException();
+        }
+    }
+
     // ---- MENU ----
     private void showMenu() {
         System.out.println("----- BANK SYSTEM -----");
         System.out.println("1 - Criar conta");
-        System.out.println("2 - Ver detalhes da conta");
+        System.out.println("2 - Listar contas");
         System.out.println("3 - Sacar");
         System.out.println("4 - Depositar");
         System.out.println("5 - Transferir");
@@ -65,58 +95,52 @@ public class ViewBank {
 
     // ---- FLOWS ----
     private void createAccountFlow() {
-        System.out.print("Documento do usuário: ");
-        String document = getStringValue();
-
         System.out.print("Saldo inicial: ");
         double balance = getDoubleValue();
 
-        Account account = bankService.createAccount(document, balance);
+        Account account = bankService.createAccount(this.loggedUser, balance);
         formatAccountInfo(account);
     }
 
     private void showAccountFlow() {
-        System.out.print("Documento do usuário: ");
-        String document = getStringValue();
-
-        Account account = bankService.getAccountByDocument(document);
+        List<Account> account = bankService.getAllUserAccounts(this.loggedUser);
         formatAccountInfo(account);
     }
 
     private void depositFlow() {
-        System.out.print("Documento do usuário: ");
-        String document = getStringValue();
+        System.out.print("Informe o número da conta: ");
+        long accountCode = getLongValue();
 
-        System.out.print("Valor para depósito: ");
+        System.out.print("Informe o valor para depósito: ");
         double value = getDoubleValue();
 
-        bankService.deposit(document, value);
-        System.out.println("Depósito realizado com sucesso.");
+        bankService.deposit(accountCode, value);
+        System.out.println("Depósito realizado com sucesso!");
     }
 
     private void withdrawFlow() {
-        System.out.print("Documento do usuário: ");
-        String document = getStringValue();
+        System.out.print("Informe o número da conta: ");
+        long accountCode = getLongValue();
 
-        System.out.print("Valor para saque: ");
+        System.out.print("Informe o valor para saque: ");
         double value = getDoubleValue();
 
-        bankService.withdraw(document, value);
-        System.out.println("Saque realizado com sucesso.");
+        bankService.withdraw(accountCode, value);
+        System.out.println("Saque realizado com sucesso!");
     }
 
     private void transferFlow() {
-        System.out.print("Documento da conta origem: ");
-        String origin = getStringValue();
+        System.out.print("Informe o número da conta de origem: ");
+        long origin = getLongValue();
 
-        System.out.print("Documento da conta destino: ");
-        String destination = getStringValue();
+        System.out.print("Informe o número da conta de destino: ");
+        long destination = getLongValue();
 
-        System.out.print("Valor da transferência: ");
+        System.out.print("Informe o valor da transferência: ");
         double value = getDoubleValue();
 
         bankService.transfer(origin, destination, value);
-        System.out.println("Transferência realizada com sucesso.");
+        System.out.println("Transferência realizada com sucesso!");
     }
 
     // ---- HELPERS ----
@@ -147,5 +171,11 @@ public class ViewBank {
     private void formatAccountInfo(Account account) {
         System.out.println("----- ACCOUNT INFO -----");
         System.out.println(account);
+    }
+
+    private void formatAccountInfo(List<Account> accounts) {
+        for (Account account : accounts) {
+            this.formatAccountInfo(account);
+        }
     }
 }
