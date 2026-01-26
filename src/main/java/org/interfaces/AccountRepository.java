1c777fbd3f1412ad1ef7b5f4100d0dfb59570336
Samuel Dias Oliveira <samueldias004@gmail.com>
2026-01-26 20:53:51 -0300

    feat: add filter account by user to repository

--- src/main/java/org/interfaces/AccountRepository.java
+++ src/main/java/org/interfaces/AccountRepository.java
@@ -1,14 +1,18 @@
 package org.interfaces;
 
 import org.model.Account;
+import org.model.User;
 
 import java.util.Optional;
+import java.util.List;
 
 public interface AccountRepository {
 
     public Account create(Account account);
 
-    public Optional<Account> getByUserDocument(String document);
+    public Optional<Account> findByCode(long accountCode);
 
     public Account update(Account account);
+
+    public List<Account> findAllUserAccounts(User user);
 }
--- src/main/java/org/repository/InMemoryAccountRepository.java
+++ src/main/java/org/repository/InMemoryAccountRepository.java
@@ -2,25 +2,33 @@ package org.repository;
 
 import org.interfaces.AccountRepository;
 import org.model.Account;
+import org.model.User;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class InMemoryAccountRepository implements AccountRepository {
-    private final Map<String, Account> accounts = new HashMap<>();
+    private final Map<Long, Account> accounts = new HashMap<>();
 
     public Account create(Account account) {
-        accounts.put(account.getOwnerDocument(), account);
+        accounts.put(account.getAccountCode(), account);
         return account;
     }
 
-    public Optional<Account> getByUserDocument(String document) {
-        return Optional.ofNullable(accounts.get(document));
+    public Optional<Account> findByCode(long accountCode) {
+        return Optional.ofNullable(accounts.get(accountCode));
     }
-
 
     public Account update(Account account) {
-        accounts.put(account.getOwnerDocument(), account);
+        accounts.put(account.getAccountCode(), account);
         return account;
+    }
+
+    public List<Account> findAllUserAccounts(User user) {
+        return this.accounts.values()
+                .stream()
+                .filter(account -> account.getOwner().getCpf().equals(user.getCpf()))
+                .collect(Collectors.toList());
     }
 
 }
