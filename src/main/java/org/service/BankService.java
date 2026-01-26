18c697a57c5b44cbe1b6b611bd9b68fb70314281
Samuel Dias Oliveira <samueldias004@gmail.com>
2026-01-26 20:56:19 -0300

    feat: adapt service to use user-based account

--- src/main/java/org/service/BankService.java
+++ src/main/java/org/service/BankService.java
@@ -1,10 +1,11 @@
 package org.service;
 
-import org.exceptions.AccountAlreadyExistsException;
 import org.exceptions.AccountNotFoundException;
 import org.interfaces.AccountRepository;
 import org.model.Account;
+import org.model.User;
 
+import java.util.List;
 import java.util.Optional;
 
 public class BankService {
@@ -14,32 +15,35 @@ public class BankService {
         this.accountRepository = accountRepository;
     }
 
-    public Account createAccount(String document, double initialBalance) {
-        if (this.accountRepository.getByUserDocument(document).isPresent()) throw new AccountAlreadyExistsException("Já existe uma conta vinculada a este documento!");
-
-        Account newAccount = new Account(document, initialBalance);
+    public Account createAccount(User user, double initialBalance) {
+        Account newAccount = new Account(user, initialBalance);
         return this.accountRepository.create(newAccount);
     }
 
-    public Account getAccountByDocument(String document) {
-        Optional<Account> acc = this.accountRepository.getByUserDocument(document);
-        if (acc.isEmpty()) throw new AccountNotFoundException();
-        return acc.get();
+    public List<Account> getAllUserAccounts(User user) {
+        List<Account> accounts = this.accountRepository.findAllUserAccounts(user);
+        if (accounts.isEmpty()) throw new AccountNotFoundException("Nenhuma conta foi encontrada!");
+        return accounts;
     }
 
-    public void deposit(String document, double value) {
-        Account account = this.getAccountByDocument(document);
-        account.deposit(value);
+    public void deposit(long accountCode, double value) {
+        Optional<Account> account = this.accountRepository.findByCode(accountCode);
+        if (account.isEmpty()) throw new AccountNotFoundException();
+        account.get().deposit(value);
     }
 
-    public void withdraw(String document, double value) {
-        Account account = this.getAccountByDocument(document);
-        account.withdraw(value);
+    public void withdraw(long accountCode, double value) {
+        Optional<Account> account = this.accountRepository.findByCode(accountCode);
+        if (account.isEmpty()) throw new AccountNotFoundException();
+        account.get().withdraw(value);
     }
 
-    public void transfer(String originDocumentOwner, String destinationDocumentOwner, double value) {
-        Account originAccount = this.getAccountByDocument(originDocumentOwner);
-        Account destinationAccount = this.getAccountByDocument(destinationDocumentOwner);
+    public void transfer(long originAccountCode, long destinationAccountCode, double value) {
+        Account originAccount = this.accountRepository.findByCode(originAccountCode)
+                .orElseThrow(() -> new AccountNotFoundException("Conta de origem não encontrada!"));
+
+        Account destinationAccount = this.accountRepository.findByCode(destinationAccountCode)
+                .orElseThrow(() -> new AccountNotFoundException("Conta de destino não encontrada!"));
 
         // Should have a transaction here
         originAccount.withdraw(value);
